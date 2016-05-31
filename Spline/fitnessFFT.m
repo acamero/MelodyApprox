24963ecaba6da6c5ca9b9aa7e8720e5f4c12b5a4
Andrés Camero <andrescamero@gmail.com>
2016-05-31 17:31:39 +0200

    Add FFT interp

---
+++ Spline/fitnessFFT.m
@@ -0,0 +1,48 @@
+function fit = fitnessFFT(filePath)
+
+rawin = csvread (filePath);
+
+% get just the positive frequencies
+rawin(:,3)=max(rawin(:,2),0);
+% calculate the midi pitch
+rawin(:,4) = floor(12*log2(rawin(:,3)/440) + 69);
+% remove Inf
+rawin(:,4) = max(rawin(:,4),0);
+% get nonzero rows, i.e. the melody
+melody = rawin(all(rawin,2),[1,4]);
+% calculate the period of each sample
+dt = rawin(2)-rawin(1);
+% calculate the number of phrases, by counting the total number
+% of "jumps" in time
+temp = melody(2:length(melody),1)-melody(1:length(melody)-1,1);
+temp2 = ones(length(temp),1)*dt*1.5;
+temp3 = temp - temp2;
+
+fitness0 = 0;
+fitness1 = 0;
+fitness2 = 0;
+fitness3 = 0;
+fitness4 = 0;
+fitness5 = 0;
+phrases = 0;
+
+j = 1;
+for i=1:length(temp3),
+
+   if temp3(i)>0,
+    % t = melody(j:i,1);    
+    pitch = melody(j:i,2);
+    n = length(pitch);
+    
+    y = real(floor( interpft( pitch, n) ));    
+    fitness0 += sum(power(pitch-y,2));    
+    j = i+1;   
+    phrases++;
+   endif
+
+endfor
+
+
+fit = [fitness0 fitness1 fitness2 fitness3 fitness4 fitness5 phrases]
+
+endfunction
